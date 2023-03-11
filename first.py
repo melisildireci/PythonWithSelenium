0f93d4384f2df23bd5c689cb51439f30b69f1d31
Melis İldireci <56775276+melisildireci@users.noreply.github.com>
2023-03-11 14:59:01 +0300

    Add files via upload

---
+++ first.py
@@ -0,0 +1,35 @@
+
+# 1-)  Python'da Veri Tiplerini araştırınız, her bir veri tipi için kendi cümlelerinizle açıklamalar yazın
+
+# String Veri Tipi : Metinsel veri tipleri için kullanılır . Örnek = 'melis'
+
+# Integer (int) Veri Tipi : Tam sayı değerleri için kullanılır. örnek =10
+
+# Float Veri Tipi : Ondalıklı sayı değerleri için kullanılır. örnek = 10.9
+
+# Boolean Veri Tipi : Mantıksal bir veri tipidir. (true or false)
+
+
+# 2-) Kodlama.io sitesinde değişken olarak kullanıldığını düşündüğünüz verileri, veri tipleriyle birlikte örneklendiriniz.
+
+# String = Eğitmen Adı
+# Integer  : Kursun tamamlanma oranı 
+
+
+
+
+# 3-) Kodlama.io sitesinde şart blokları kullanıldığını düşündüğünüz kısımları örneklendiriniz ve Python dilinde bu örnekleri koda dökünüz.
+
+
+
+
+email = input("Mail adresinizi giriniz\n")
+sifre = int(input("Şifrenizi giriniz\n"))
+
+
+if email == ("melis@123") and sifre == (123):
+    print("Başarıyla giriş yapıldı")
+
+
+else:
+    print("Mail ya da şifre hatali!")
